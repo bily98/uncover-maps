be47560151bf082db1577897af98a18659d4c0b0
Bily Venerio <bilyjv98.16@gmail.com>
2020-10-10 01:59:13 -0600

    Vuforia

---
+++ app/src/main/java/com/ar/uncovermaps/Utils.kt
@@ -0,0 +1,22 @@
+package com.ar.uncovermaps
+
+import android.content.Context
+import java.io.File
+
+class Utils {
+    companion object {
+        private const val KEY_SH_PRE_FILE = "KEY_SH_PRE_FILE"
+
+        @JvmStatic
+        fun getLastSavedModelUrl(context: Context, key: String): String? {
+            val sharedPreferences =
+                context.getSharedPreferences(KEY_SH_PRE_FILE, Context.MODE_PRIVATE)
+            return sharedPreferences.getString(key, null)
+        }
+
+        fun isFileExist(context: Context, fileName: String): Boolean {
+            val file = File(context.filesDir.toString() + "/" + fileName)
+            return file.exists()
+        }
+    }
+}
